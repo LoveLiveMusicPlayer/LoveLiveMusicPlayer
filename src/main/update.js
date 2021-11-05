4de43fdf61c217e9f4b2549e04affb59b878e48f
gr.zhu <55681140@163.com>
2021-11-05 15:56:54 +0800

    打包版本获取资源路径

--- src/main/update.js
+++ src/main/update.js
@@ -1,11 +1,20 @@
-import {dialog} from 'electron'
+import {app, dialog} from 'electron'
 import {autoUpdater} from 'electron-updater'
 import http from 'http'
-import icon from '../../assets/icon.png'
+import path from "path";
 
 // see  https://www.electron.build/auto-update#events
 autoUpdater.on('update-downloaded', info => {
     if (process.env.NODE_ENV === 'production') {
+
+        const RESOURCES_PATH = app.isPackaged
+            ? path.join(process.resourcesPath, 'assets')
+            : path.join(__dirname, '../../assets');
+
+        const getAssetPath = (path) => {
+            return path.join(RESOURCES_PATH, ...path);
+        };
+
         // https://electronjs.org/docs/api/auto-updater#autoupdaterquitandinstall
         // 这里先拉取更新信息，在对话框中显示版本的更新内容
         const req = http.request('http://192.168.100.194:8080/info.txt', req => {
@@ -17,7 +26,7 @@ autoUpdater.on('update-downloaded', info => {
             req.on('end', () => {
                 dialog.showMessageBox(
                     {
-                        icon: icon,
+                        icon: getAssetPath("icon.png"),
                         type: 'info',
                         title: '软件更新',
                         message: `已更新到最新版本（${info.version}）请重启应用。`,
