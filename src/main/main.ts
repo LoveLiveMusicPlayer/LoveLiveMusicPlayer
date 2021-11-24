da014ecf97a502f018368634c8ef51eaad1552d0
zhushenwudi <55681140@163.com>
2021-11-24 11:58:39 +0800

    禁用浏览器缓存

--- src/main/main.ts
+++ src/main/main.ts
@@ -95,6 +95,8 @@ ipcMain.handle("checkUpdate", (_event, _args) => {
     autoUpdater.checkUpdate(updateCallback)
 })
 
+app.commandLine.appendSwitch("--disable-http-cache")
+
 if (process.env.NODE_ENV === 'production') {
     const sourceMapSupport = require('source-map-support');
     sourceMapSupport.install();
