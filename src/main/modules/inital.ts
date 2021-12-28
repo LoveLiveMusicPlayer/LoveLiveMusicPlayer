d94947f9f1f24508db7677df3f0f4e2fc529d433
zhushenwudi <55681140@163.com>
2021-12-28 18:38:28 +0800

    上报位置变更

--- src/main/modules/inital.ts
+++ src/main/modules/inital.ts
@@ -1,5 +1,8 @@
-import {app} from "electron";
+import {app, crashReporter} from "electron";
 import path from "path";
+import * as Sentry from "@sentry/electron";
+import {SENTRY_URL} from "../../renderer/utils/URLHelper";
+import {machineId} from "node-machine-id";
 
 const Store = require('electron-store');
 
@@ -12,7 +15,7 @@ export const RESOURCES_PATH = app.isPackaged
     : path.join(__dirname, '../../../assets');
 
 export default function () {
-
+    Sentry.init({dsn: SENTRY_URL});
     app.commandLine.appendSwitch("--disable-http-cache")
     app.commandLine.appendSwitch('wm-window-animations-disabled')
 
@@ -27,4 +30,21 @@ export default function () {
     if (isDevelopment) {
         require('electron-debug')();
     }
+
+    crashReporter.start({
+        productName: "LoveLiveMusicPlayer",
+        companyName: "zhushenwudi",
+        submitURL: "http://139.224.116.225:9000/api/6/minidump/?sentry_key=1d2d5d17044544e8b37e78182a2f1c77",
+        extra:{
+            "isDev": isDevelopment ? 'yes' : 'no',
+            "version": app.getVersion()
+        }
+    });
+
+    setTimeout(() => {
+        // 上报电脑唯一标识
+        machineId(true).then(id => {
+            Sentry.captureMessage(id)
+        })
+    }, 1000)
 }
