d7914f63457d979f998e02e306efb4d2eb5334e6
zhushenwudi <55681140@163.com>
2022-08-16 16:07:14 +0800

    测试打包1.3.0

--- src/renderer/utils/VersionUtils.js
+++ src/renderer/utils/VersionUtils.js
@@ -14,17 +14,17 @@ const isNeedInit = {needInit: true, status: 0}
 export const VersionUtils = {
     // 获取连接桥跳板
     getBridgeUrl(appVersion) {
-        return OWNER_OSS_URL_HEAD + appVersion + isPre ? "/pre/" : "/prod/" + BRIDGE_URL
+        return OWNER_OSS_URL_HEAD + appVersion + (isPre ? "/pre/" : "/prod/") + BRIDGE_URL
     },
 
     // 更新数据的地址
     getRefreshDataUrl(bridgeUrl, appVersion) {
-        return bridgeUrl + appVersion + isPre ? "/pre/" : "/prod/" + DATA_FILE
+        return bridgeUrl + appVersion + (isPre ? "/pre/" : "/prod/") + DATA_FILE
     },
 
     // 更新版本的地址
     getVersionInfo() {
-        return OWNER_OSS_URL_HEAD + isPre ? "version/pre/" : "version/prod/" + VERSION_FILE
+        return OWNER_OSS_URL_HEAD + (isPre ? "version/pre/" : "version/prod/") + VERSION_FILE
     },
 
     // 获取版本初始化模式
@@ -34,6 +34,6 @@ export const VersionUtils = {
 
     // 获取开屏提醒的地址
     getVersionHintUrl(appVersion) {
-        return OWNER_OSS_URL_HEAD + appVersion + isPre ? "/pre/" : "/prod/" + REQUEST_LATEST_VERSION_FILE
+        return OWNER_OSS_URL_HEAD + appVersion + (isPre ? "/pre/" : "/prod/") + REQUEST_LATEST_VERSION_FILE
     }
 }
